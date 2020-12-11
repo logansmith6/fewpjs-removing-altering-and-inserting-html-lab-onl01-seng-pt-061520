1222ed08ff23a5b8eb2ace29ce3289d16db43597
Logan Smith <logsmith06@gmail.com>
2020-12-11 18:00:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.classList.add("victory");
+newHeader.id = "victory"
+newHeader.innerHTML = 'YOUR-NAME is the champion'
